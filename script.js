e289b10c8ffa0b0d8f9cc4a629c0da7fe072b27b
nguyet33 <tdnguyet33@gmail.com>
2023-03-30 20:39:04 -0700

    adding more question and adjusting start time

--- script.js
+++ script.js
@@ -25,10 +25,70 @@ var quiz = [
     ],
     answer: "alert('Hello World');",
   },
+  {
+    question: "What is my name?",
+    choices: ["Thien Smith", "John Doe", "Adam Sander", "Magic Johnson"],
+    answer: "Thien Smith",
+  },
+  {
+    question: "What is the right answer?",
+    choices: ["wrong", "wrong", "right", "Not right"],
+    answer: "right",
+  },
+  {
+    question: "Select the right letter in in Adam?",
+    choices: ["A", "d", "m", "s"],
+    answer: "A",
+  },
+  {
+    question: "When was Thien born?",
+    choices: ["1998", "1786", "1997", "1970"],
+    answer: "1970",
+  },
+  {
+    question: "What does the acronym 'DOM' stand for?",
+    choices: [
+      "Document Object Model",
+      "Dynamic Object Module",
+      "Digital Object Management",
+      "Data Object Manager",
+    ],
+    answer: "Document Object Model",
+  },
+  {
+    question: "What does the acronym 'HTML' stand for?",
+    choices: [
+      "Hello The Make laugh",
+      "Hi This My Learning",
+      "Hello Thien Must Laugh",
+      "Hyper Text Markup Langauge",
+    ],
+    answer: "Hyper Text Markup Langauge",
+  },
+  {
+    question: "Which of the following is not a JavaScript data type?",
+    choices: ["string", "number", "boolean", "float"],
+    answer: "float",
+  },
+  {
+    question: "What does the 'this' keyword refer to in JavaScript?",
+    choices: [
+      "The global object",
+      "The object that called the function",
+      "The object being created",
+      "The object that contains the function",
+    ],
+    answer: "The object that called the function",
+  },
+  {
+    question: "What is the right answer?",
+    choices: ["wrong", "wrong", "right", "Not right"],
+    answer: "right",
+  }
 ];
 
 var questionIndex = 0;
-var time = quiz.length *20 ; // 60 sec start
+var time = quiz.length *5 ; // 60 sec start
 var timerId;
 
 let highscores = [];
